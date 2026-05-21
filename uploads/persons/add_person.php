9688c4a09b6fa68426905cf4806084bf52b86a6e
Mahesh Ugale <patilmahi0711@gmail.com>
2026-05-21 13:00:46 +0530

    Removed personal and user image data

--- uploads/persons/add_person.php
+++
@@ -1,5 +0,0 @@
-<form action="save_person.php" method="post" enctype="multipart/form-data">
-    <input name="name" placeholder="Person Name" required><br>
-    <input type="file" name="image" required><br>
-    <button>Save</button>
-</form>
--- uploads/persons/view_person.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../config/db.php";
-$q = $conn->query("SELECT * FROM persons");
-
-while ($r = $q->fetch_assoc()) {
-    echo "<p>{$r['name']}</p>";
-    echo "<img src='../uploads/persons/{$r['image']}' width='120'><hr>";
-}
-?>
